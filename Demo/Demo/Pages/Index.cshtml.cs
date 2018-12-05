645eada9a7f38f888e7a266e75c08f36f568730d
Роман Булдыгин <roman.buldygin@skbkontur.ru>
2018-12-05 16:55:45 +0300

    AspNet Logging

--- Demo/Demo/Pages/Index.cshtml.cs
+++ Demo/Demo/Pages/Index.cshtml.cs
@@ -4,14 +4,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace Demo.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ILogger _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
         public void OnGet()
         {
 
+        }
+
+        public void OnPostLogError()
+        {
+            _logger.LogError("Какая-то ошибка произошла");
+        }
+
+        public void OnPostCreateException()
+        {
+            throw new ArgumentOutOfRangeException("testArg", "Wtf with arg");
         }
     }
 }
--- Demo/Demo/Program.cs
+++ Demo/Demo/Program.cs
@@ -19,6 +19,7 @@ namespace Demo
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseSentry();
     }
 }
